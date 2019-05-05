6f86565bb408f4a75d01fa76c9e4faf8d317e2fe
Administrator <Administrator@SKY-20181221LEC>
2019-05-05 19:53:46 +0800

    1

--- src/main/java/com/yc/SellStore/SellStoreApplication.java
+++ src/main/java/com/yc/SellStore/SellStoreApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SellStoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SellStoreApplication.class, args);
+		SpringApplication.run(SellStoreApplication.class, args); 
 	}
 
 }
